ba8b41ead5944fe39054b65da72ad028610b0beb
satish <satishpunekar27@gmail.com>
2024-07-22 23:59:26 +0530

    First Changes

---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> { 
+            new VillaDTO {Id=1, Name="Pool view", Occupancy=20, Sqft=1500 },
+            new VillaDTO {Id=2, Name="Beach view", Occupancy=30, Sqft=3000 }
+            };
+    }
+}
